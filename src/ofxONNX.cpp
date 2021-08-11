cd6a1a1501bc0b228d197c0f7333f22102518b22
yuta okuyama <yutaokuyam@gmail.com>
2021-08-12 05:09:07 +0900

    Impl minimum wrappers

---
+++ src/ofxONNX.cpp
@@ -0,0 +1,34 @@
+#include "ofxONNX.h"
+
+ofxONNXModel::ofxONNXModel(const ORTCHAR_T* modelName, bool useCUDA)
+{
+	try {
+		Ort::SessionOptions sessionOption;
+		if (useCUDA) {
+			enableCUDA(sessionOption);
+		}
+
+		session_ = new Ort::Session(env, modelName, sessionOption);
+	}
+	catch (const Ort::Exception& exception) {
+		std::cerr << exception.what() << std::endl;
+	}
+
+}
+
+Ort::Session* ofxONNXModel::getSession() {
+	return session_;
+}
+
+void ofxONNXModel::forward(const Ort::RunOptions& run_options, const char* const* input_names, const Ort::Value* input_values, size_t input_count,
+	const char* const* output_names, Ort::Value* output_values, size_t output_count) {
+	session_->Run(Ort::RunOptions{ nullptr }, input_names, input_values, input_count, output_names, output_values, output_count);
+
+
+}
+
+
+void ofxONNXModel::enableCUDA(Ort::SessionOptions& options) {
+	OrtCUDAProviderOptions cuda_option = OrtCUDAProviderOptions{ 0 };
+	options.AppendExecutionProvider_CUDA(cuda_option);
+}
---
+++ src/ofxONNX.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ofMain.h"
+#include "onnxruntime_cxx_api.h"
+
+class ofxONNXModel {
+public:
+	ofxONNXModel(const ORTCHAR_T* modelName, bool useCUDA);
+	Ort::Session* getSession();
+
+	void forward(const Ort::RunOptions& run_options, const char* const* input_names, const Ort::Value* input_values, size_t input_count,
+		const char* const* output_names, Ort::Value* output_values, size_t output_count);
+
+private:
+	void enableCUDA(Ort::SessionOptions& options);
+
+	Ort::Env env;
+	Ort::Session* session_;
+
+};
