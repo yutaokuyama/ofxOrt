77a8b8c9f8413ec7ba7d0cebffb5f5b68b301fdb
yuta okuyama <yutaokuyam@gmail.com>
2021-08-13 23:49:00 +0900

    Add empty pose estimation example

---
+++ examples/poseEstimation/src/ofApp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOrt.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+
+
+		ofxOrt* ort;
+
+		
+};
