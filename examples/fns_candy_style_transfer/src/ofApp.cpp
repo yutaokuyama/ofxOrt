516ea9f07e189d653a26d85147554916be754635
yuta okuyama <yutaokuyam@gmail.com>
2021-08-13 17:03:57 +0900

    Refactor style transfer example

--- examples/fns_candy_style_transfer/src/ofApp.cpp
+++ examples/fns_candy_style_transfer/src/ofApp.cpp
@@ -1,24 +1,24 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 	const ORTCHAR_T* modelName = L"candy.onnx";
 	ort = new ofxOrt(modelName, true);
 
-	fbo.allocate(720, 720,GL_RGB);
+	fbo.allocate(720, 720, GL_RGB);
 	fbo.begin();
 	ofClear(0, 255);
-
 	fbo.end();
-}
-
-//--------------------------------------------------------------
-void ofApp::update(){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
 
 
 	fbo.begin();
@@ -35,11 +35,10 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 	if (key == ' ') {
 		fbo.begin();
 		ofClear(0, 255);
-
 		fbo.end();
 	}
 }
@@ -52,29 +51,23 @@ void ofApp::inference(ofFloatImage content) {
 
 	auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
 
-	ofImage result("sample.jpg");
-	result.resize(720, 720);
-	result.update();
-
 	ofFloatPixels pix;
 	ofFloatPixels pix_result;
 
 	content.getTexture().readToPixels(pix);
 	content.getTexture().readToPixels(pix_result);
 
-
 	ofxOrtImageTensor input_tensor = ofxOrtImageTensor(memory_info, content.getTexture());
 	ofxOrtImageTensor output_tensor = ofxOrtImageTensor(memory_info, content.getTexture());
 
-
 	ort->forward(Ort::RunOptions{ nullptr }, input_names, &(input_tensor.getTensor()), 1, output_names, &(output_tensor.getTensor()), 1);
-	
-	pix.setFromAlignedPixels(output_tensor.getTexData().data(), 720, 720, OF_PIXELS_RGB,720*3);
 
-	ofxOrtUtils::chw_to_hwc(pix, pix_result, 720, 720);
+	pix.setFromAlignedPixels(output_tensor.getTexData().data(), content.getWidth(), content.getHeight(), OF_PIXELS_RGB, content.getHeight() * 3);
 
+	ofxOrtUtils::chw_to_hwc(pix, pix_result, content.getWidth(), content.getHeight(), true);
+
+	ofImage result;
 	result.setFromPixels(pix_result);
-
 	result.update();
 	result.draw(720.0, 0.0);
 }
--- examples/fns_candy_style_transfer/src/ofApp.h
+++ examples/fns_candy_style_transfer/src/ofApp.h
@@ -2,22 +2,19 @@
 
 #include "ofMain.h"
 #include "ofxOrt.h"
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
-		void keyPressed(int key);
+public:
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
 
-		void inference(ofFloatImage content);
-		ofxOrt* ort;
+	void inference(ofFloatImage content);
+	ofxOrt* ort;
 
-		ofImage img;
-		
-		ofFloatPixels pixCHW;
-		glm::vec2 originalSize;
+	ofFloatPixels pixCHW;
 
-		ofFbo fbo;
-		ofVideoGrabber grabber;
+	ofFbo fbo;
+	ofVideoGrabber grabber;
 };
