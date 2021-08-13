ed1d1dd68fd387cd27e903bf858ad0d6ca60eaac
yuta okuyama <yutaokuyam@gmail.com>
2021-08-13 18:13:59 +0900

    Add getter for model information

--- src/ofxOrt.h
+++ src/ofxOrt.h
@@ -12,10 +12,26 @@ public:
 	void forward(const Ort::RunOptions& run_options, const char* const* input_names, const Ort::Value* input_values, size_t input_count,
 		const char* const* output_names, Ort::Value* output_values, size_t output_count);
 
+
+	const size_t getSessionInputCount();
+	const size_t getSessionOutputCount();
+	const string getSessionInputName();
+	const string getSessionOutputName();
+
+	const ONNXTensorElementDataType getSessionInputType();
+	const ONNXTensorElementDataType getSessionOutputType();
+
+	const std::vector<int64_t> getInputDims();
+	const std::vector<int64_t> getOutputDims();
+
+	void printModelInfo();
+
 private:
 	void enableCUDA(Ort::SessionOptions& options);
 
 	Ort::Env env;
 	Ort::Session* session_;
+	Ort::AllocatorWithDefaultOptions defaultAllocator_;
+
 
 };
