97c1bda6f3a98dc44ae4bb60628ebcdcf2d25fd3
yuta okuyama <yutaokuyam@gmail.com>
2021-08-13 17:04:07 +0900

    Apply formatter

--- src/ofxOrtTensors.h
+++ src/ofxOrtTensors.h
@@ -2,51 +2,51 @@
 #include "ofMain.h"
 #include "onnxruntime_cxx_api.h"
 
-class ofxOrtImageTensor{
-	public:
-		ofxOrtImageTensor(Ort::MemoryInfo& memInfo, ofTexture& tex, bool isGrayscale=false ) :tensor( nullptr )
-		{
-			ofFloatPixels pix;
-			tex.readToPixels(pix);
-			if (isGrayscale) {
-				pix.setImageType(OF_IMAGE_GRAYSCALE);
-			}
-			texData = std::vector<float>{pix.getData(), pix.getData() + pix.size() };
-			data_shape = std::array<int64_t, 4>{ 1, int(pix.getNumChannels()), int(tex.getWidth()), int(tex.getHeight()) };
-			tensor = Ort::Value::CreateTensor<float>(memInfo, texData.data(), texData.size(), data_shape.data(), data_shape.size());
+class ofxOrtImageTensor {
+public:
+	ofxOrtImageTensor(Ort::MemoryInfo& memInfo, ofTexture& tex, bool isGrayscale = false) :tensor(nullptr)
+	{
+		ofFloatPixels pix;
+		tex.readToPixels(pix);
+		if (isGrayscale) {
+			pix.setImageType(OF_IMAGE_GRAYSCALE);
 		}
-		Ort::Value& getTensor(){
-			return tensor;
-		}
+		texData = std::vector<float>{ pix.getData(), pix.getData() + pix.size() };
+		data_shape = std::array<int64_t, 4>{ 1, int(pix.getNumChannels()), int(tex.getWidth()), int(tex.getHeight()) };
+		tensor = Ort::Value::CreateTensor<float>(memInfo, texData.data(), texData.size(), data_shape.data(), data_shape.size());
+	}
+	Ort::Value& getTensor() {
+		return tensor;
+	}
 
-		std::vector<float>& getTexData() {
-			return texData;
-		};
-	private:
-		Ort::Value tensor;
-		std::vector<float> texData;
-		std::array<int64_t, 4> data_shape;
+	std::vector<float>& getTexData() {
+		return texData;
+	};
+private:
+	Ort::Value tensor;
+	std::vector<float> texData;
+	std::array<int64_t, 4> data_shape;
 };
 
 class ofxOrt1DTensor {
-	public:
-		ofxOrt1DTensor(Ort::MemoryInfo& memInfo,int numValue)
+public:
+	ofxOrt1DTensor(Ort::MemoryInfo& memInfo, int numValue)
 		:tensor_(nullptr)
-		{
-			data_.resize(numValue);
-			data_shape_ = std::array<int64_t, 2>{ 1, int(data_.size()) };
-			tensor_ = Ort::Value::CreateTensor<float>(memInfo, data_.data(), data_.size(), data_shape_.data(), data_shape_.size());
-		}
+	{
+		data_.resize(numValue);
+		data_shape_ = std::array<int64_t, 2>{ 1, int(data_.size()) };
+		tensor_ = Ort::Value::CreateTensor<float>(memInfo, data_.data(), data_.size(), data_shape_.data(), data_shape_.size());
+	}
 
-		Ort::Value& getTensor() {
-			return tensor_;
-		}
+	Ort::Value& getTensor() {
+		return tensor_;
+	}
 
-		std::vector<float> getData(){
-			return data_;
-		}
-	private:
-		std::vector<float> data_;
-		Ort::Value tensor_;
-		std::array<int64_t, 2> data_shape_;
+	std::vector<float> getData() {
+		return data_;
+	}
+private:
+	std::vector<float> data_;
+	Ort::Value tensor_;
+	std::array<int64_t, 2> data_shape_;
 };
--- src/ofxOrtUtils.h
+++ src/ofxOrtUtils.h
@@ -4,59 +4,71 @@
 
 class ofxOrtUtils {
 public:
-    //TODO::extremely inefficient
-    static void hwc_to_chw(const ofTexture& tex, ofFloatPixels& pixels_chw) {
+	//TODO::extremely inefficient
+	static void hwc_to_chw(const ofTexture& tex, ofFloatPixels& pixels_chw, bool normalize = false) {
 
-        ofFloatPixels pixels_hwc;
-        tex.readToPixels(pixels_hwc);
-        tex.readToPixels(pixels_chw);
-        int stride = int(int(tex.getHeight()) * int(tex.getWidth()));
-        std::cout << int(stride) << std::endl;
-        for (int i = 0; i != stride; ++i) {
-            for (int c = 0; c != 3; ++c) {
-                pixels_chw[c * stride + i] = pixels_hwc[i * 3 + c];
+		ofFloatPixels pixels_hwc;
+		pixels_chw.allocate(tex.getWidth(), tex.getHeight(), OF_IMAGE_COLOR);
+		tex.readToPixels(pixels_hwc);
+		int stride = int(int(tex.getHeight()) * int(tex.getWidth()));
+
+		for (int i = 0; i != stride; ++i) {
+			for (int c = 0; c != 3; ++c) {
+				pixels_chw[c * stride + i] = pixels_hwc[i * 3 + c];
+				if (normalize) {
+					pixels_hwc[i * 3 + c] /= 255.0;
+				}
 			}
-        }
-    }
+		}
+	}
 
-    static void hwc_to_chw(const ofFloatPixels& pixels_hwc, ofFloatPixels& pixels_chw, int width, int height) {
+	static void hwc_to_chw(const ofFloatPixels& pixels_hwc, ofFloatPixels& pixels_chw, int width, int height, bool normalize = false) {
 
-        int stride = width * height;
-        for (int i = 0; i != stride; ++i) {
-            for (int c = 0; c != 3; ++c) {
-                pixels_chw[c * stride + i] = pixels_hwc[i * 3 + c];
-            }
-        }
-    }
+		int stride = width * height;
+		for (int i = 0; i != stride; ++i) {
+			for (int c = 0; c != 3; ++c) {
+				pixels_chw[c * stride + i] = pixels_hwc[i * 3 + c];
+				if (normalize) {
+					pixels_chw[c * stride + i] /= 255.0;
+				}
+			}
+		}
+	}
 
-    static void chw_to_hwc(const ofTexture& tex, ofFloatPixels& pixels_hwc) {
+	static void chw_to_hwc(const ofTexture& tex, ofFloatPixels& pixels_hwc, bool normalize = false) {
 
-        ofFloatPixels pixels_chw;
-        tex.readToPixels(pixels_hwc);
-        tex.readToPixels(pixels_chw);
-        int stride = int(tex.getHeight() * tex.getWidth());
+		ofFloatPixels pixels_chw;
+		tex.readToPixels(pixels_hwc);
+		tex.readToPixels(pixels_chw);
+		int stride = int(tex.getHeight() * tex.getWidth());
 
-        for (int c = 0; c != 3; ++c) {
-            int t = c * stride;
-            for (int i = 0; i != stride; ++i) {
-                float f = pixels_chw[t + i];
-                if (f < 0.f || f > 255.0f) f = 0;
-                pixels_hwc[i * 3 + c] = f;
-            }
-        }
-    }
-    static void chw_to_hwc(const ofFloatPixels& pixels_chw, ofFloatPixels& pixels_hwc,int width,int height) {
-        
-        int stride = int(width * height);
+		for (int c = 0; c != 3; ++c) {
+			int t = c * stride;
+			for (int i = 0; i != stride; ++i) {
+				float f = pixels_chw[t + i];
+				if (f < 0.f || f > 255.0f) f = 0;
+				pixels_hwc[i * 3 + c] = f;
+				if (normalize) {
+					pixels_hwc[i * 3 + c] /= 255.0;
+				}
+			}
+		}
+	}
+	static void chw_to_hwc(const ofFloatPixels& pixels_chw, ofFloatPixels& pixels_hwc, int width, int height, bool normalize = false) {
 
-        for (int c = 0; c != 3; ++c) {
-            int t = c * stride;
-            for (int i = 0; i != stride; ++i) {
-                float f = pixels_chw[t + i];
-                if (f < 0.f || f > 255.0f) f = 0;
-                pixels_hwc[i * 3 + c] = f/255.0;
-            }
-        }
-    }
+		int stride = int(width * height);
+
+		for (int c = 0; c != 3; ++c) {
+			int t = c * stride;
+			for (int i = 0; i != stride; ++i) {
+				float f = pixels_chw[t + i];
+				if (f < 0.f || f > 255.0f) f = 0;
+				pixels_hwc[i * 3 + c] = f;
+				if (normalize) {
+					pixels_hwc[i * 3 + c] /= 255.0;
+				}
+			}
+		}
+	}
 };
 
