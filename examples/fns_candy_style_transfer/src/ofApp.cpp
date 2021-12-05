bd7934c5424ad48e0abef9ac976a1ef7e1e9ddbe
yuta okuyama <yutaokuyam@gmail.com>
2021-12-05 22:10:07 +0900

    FIx access error

--- examples/fns_candy_style_transfer/src/ofApp.cpp
+++ examples/fns_candy_style_transfer/src/ofApp.cpp
@@ -2,83 +2,99 @@
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-  const ORTCHAR_T *modelName = L"candy.onnx";
+	const ORTCHAR_T* modelName = L"candy.onnx";
 
-  fbo.allocate(720, 720, GL_RGB);
-  fbo.begin();
-  ofClear(0, 255);
-  fbo.end();
+	fbo.allocate(720, 720, GL_RGB);
+	fbo.begin();
+	ofClear(0, 255);
+	fbo.end();
 
 
-  ort.setup(720, 720);
-  grabber.setDeviceID(1);
-  grabber.initGrabber(600,400);
-  
+	ort.setup(720, 720);
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-    grabber.update();
-    if (grabber.isFrameNew()) {
-        fbo.begin();
-        ofClear(0, 255);
-        //ofSetColor(ofRandom(0, 255), ofRandom(0, 255), ofRandom(0, 255));
-        //ofDrawCircle(mouseX, mouseY, ofRandom(10, 100));
-        grabber.draw(0.0, 0.0, fbo.getWidth(), fbo.getHeight());
-        fbo.end();
-        isFboReady = true;
-    }
-    ort.lock();
-    if (!ort._isImageSeted) {
-        setImageToModel();
-    }
-    if (ort._isImageProcessed && ort._isImageSeted) {
-        readImageFromModel();
-    }
 
-    ort.unlock();
-    if (!ort.isThreadRunning() && ort._isImageSeted && !ort._isImageProcessed) {
-        ort.startThread(true);
-    }
+	fbo.begin();
+	ofSetColor(ofRandom(0, 255), ofRandom(0, 255), ofRandom(0, 255));
+	ofDrawCircle(mouseX, mouseY, ofRandom(10, 100));
+	fbo.end();
+
+
+	//I'm not sure if this is the right implementation for exclusions.
+	if (fbo.checkStatus()) {
+
+
+		ort.lock();
+		if (!_isImageSet && !ort.isThreadRunning()) {
+			setImageToModel();
+
+		}
+		if (ort._isImageProcessed && _isImageSet) {
+			readImageFromModel();
+		}
+
+		ort.unlock();
+
+
+
+		if (!ort.isThreadRunning()) {
+			ort.startThread(false);
+		}
+
+
+
+	}
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-  fbo.draw(0.0, 0.0);
-  resultImage.draw(720.0, 0.0);
+	fbo.draw(0.0, 0.0, 360, 360);
+	resultImage.draw(720.0 / 2.0, 0.0, 360, 360);
+	ofDrawBitmapString(ofToString(ofGetFrameRate()), 0, 10);
+
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
-  if (key == ' ') {
-    fbo.begin();
-    ofClear(0, 255);
-    fbo.end();
-  }
+	if (key == ' ') {
+		fbo.begin();
+		ofClear(0, 255);
+		fbo.end();
+	}
 }
 
 void ofApp::updateResultImage() {
-    resultImage.setFromPixels(ort._dstPixels);
-    resultImage.update();
+	resultImage.setFromPixels(ort._dstPixels);
+	resultImage.update();
 }
 
 void ofApp::exit() {
-    ort.stopThread();
+
+
+	ort.stopThread();
+
+
+
 }
 
 void ofApp::setImageToModel() {
 
-    fbo.getTexture().readToPixels(ort._srcHWCPixels);
-    ort._isImageSeted = true;
-    ort._isImageProcessed = false;
+	fbo.getTexture().readToPixels(ort._srcHWCPixels);
+
+
+	_isImageSet = true;
+	ort._isImageProcessed = false;
 
 }
 
 void ofApp::readImageFromModel() {
 
-    updateResultImage();
-    ort._isImageSeted = false;
-    ort._isImageProcessed = false;
+	updateResultImage();
+	_isImageSet = false;
+	ort._isImageProcessed = false;
 }
--- examples/fns_candy_style_transfer/src/ofApp.h
+++ examples/fns_candy_style_transfer/src/ofApp.h
@@ -4,63 +4,63 @@
 #include "ofxOrt.h"
 
 
-class ThreadedInference: public ofThread {
+class ThreadedInference : public ofThread {
 public:
 
-    void setup(const int width,const int height)
-    {
-        const ORTCHAR_T* modelName = L"candy.onnx";
-        ort = new ofxOrt(modelName, true);
-        ort->printModelInfo();
-        _width = width;
-        _height = height;
-  
-        _srcHWCPixels.allocate(width, height, OF_IMAGE_COLOR);
-    }
+	void setup(const int width, const int height)
+	{
+		const ORTCHAR_T* modelName = L"candy.onnx";
+		ort = new ofxOrt(modelName, true);
+		ort->printModelInfo();
+		_srcHWCPixels.allocate(width, height, OF_IMAGE_COLOR);
+	}
 
-    void threadedFunction() override {
-        inference();
-    }
+	void threadedFunction() override {
 
-    void inference() {
-        ofFloatPixels pixCHW(_srcHWCPixels);
-        ofxOrtUtils::rgb2chw(_srcHWCPixels, pixCHW, true, true, 1.0);
-
-        const char* input_names[] = { "inputImage" };
-        const char* output_names[] = { "outputImage" };
-
-        auto memory_info =
-            Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+		inference();
+	}
 
 
-        ofxOrtImageTensor<float> input_tensor(memory_info, pixCHW, _width, _height);
-        ofxOrtImageTensor<float> output_tensor(memory_info, pixCHW, _width, _height);
+	void inference() {
 
-        ort->forward(Ort::RunOptions{ nullptr }, input_names,
-            &(input_tensor.getTensor()), 1, output_names,
-            &(output_tensor.getTensor()), 1);
+		if (_srcHWCPixels.getWidth() == 0) {
+			abort();
+		}
+		ofFloatPixels pixCHW(_srcHWCPixels);
+		ofxOrtUtils::rgb2chw(_srcHWCPixels, pixCHW, true, true, 1.0);
 
-        ofFloatPixels pix_result;
+		const char* input_names[] = { "inputImage" };
+		const char* output_names[] = { "outputImage" };
 
-        _dstPixels.setFromAlignedPixels(output_tensor.getTexData().data(),
-            pixCHW.getWidth(), pixCHW.getHeight(),
-            OF_PIXELS_RGB, pixCHW.getHeight() * 3);
+		auto memory_info =
+			Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
 
-        _dstPixels = (ofxOrtUtils::chw2hwc(_dstPixels, 1.0 / 255.0));
-       _dstPixels.swapRgb();
-       _isImageProcessed = true;
 
-    }
+		ofxOrtImageTensor<float> input_tensor(memory_info, pixCHW, _srcHWCPixels.getWidth(), _srcHWCPixels.getHeight());
+		ofxOrtImageTensor<float> output_tensor(memory_info, pixCHW, _srcHWCPixels.getWidth(), _srcHWCPixels.getHeight());
 
-    ofFloatPixels _srcHWCPixels;
-    ofFloatPixels _dstPixels;
-    bool _isImageProcessed = true;
-    bool _isImageSeted = false;
-    
+		ort->forward(Ort::RunOptions{ nullptr }, input_names,
+			&(input_tensor.getTensor()), 1, output_names,
+			&(output_tensor.getTensor()), 1);
+
+		_dstPixels.setFromAlignedPixels(output_tensor.getTexData().data(),
+			pixCHW.getWidth(), pixCHW.getHeight(),
+			OF_PIXELS_RGB, pixCHW.getHeight() * 3);
+
+		_dstPixels = (ofxOrtUtils::chw2hwc(_dstPixels, 1.0 / 255.0));
+		_dstPixels.swapRgb();
+		_isImageProcessed = true;
+
+
+	}
+
+	ofFloatPixels _srcHWCPixels;
+	ofFloatPixels _dstPixels;
+	bool _isImageProcessed = true;
+
+
 private:
-    ofxOrt* ort;
-    int _width;
-    int _height;
+	ofxOrt* ort;
 
 };
 
@@ -68,25 +68,26 @@ private:
 class ofApp : public ofBaseApp {
 
 public:
-  void setup() override;
-  void update();
-  void draw();
-  void exit() override;
-  void keyPressed(int key);
-  void updateResultImage();
+	void setup() override;
+	void update();
+	void draw();
+	void exit() override;
+	void keyPressed(int key);
+	void updateResultImage();
 
-  void setImageToModel();
-  void readImageFromModel();
+	void setImageToModel();
+	void readImageFromModel();
 
-  //ofFloatPixels inference(ofFloatPixels& content, int width, int height);
+	//ofFloatPixels inference(ofFloatPixels& content, int width, int height);
 
 
-  ofFloatPixels pixCHW;
+	ofFloatPixels pixCHW;
 
-  ofFbo fbo;
-  ofVideoGrabber grabber;
-  ThreadedInference ort;
-  ofImage resultImage;
-  bool isFboReady = false;
+	ofFbo fbo;
+	ofVideoGrabber grabber;
+	ThreadedInference ort;
+	ofImage resultImage;
+	bool isFboReady = false;
+	bool _isImageSet = false;
 
 };
