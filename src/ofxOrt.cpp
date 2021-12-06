7fc62ab9f5447bcea8e4f0f11b2f5d76010d1ed8
yuta okuyama <yutaokuyam@gmail.com>
2021-12-07 04:04:52 +0900

    Apply linter

--- src/ofxOrt.cpp
+++ src/ofxOrt.cpp
@@ -1,32 +1,42 @@
 #include "ofxOrt.h"
 
-ofxOrt::ofxOrt(const ORTCHAR_T *modelName, bool useCUDA) {
-  try {
-    Ort::SessionOptions sessionOption;
-    if (useCUDA) {
-      enableCUDA(sessionOption);
-    }
+ofxOrt::ofxOrt(const ORTCHAR_T* modelName, bool useCUDA) {
+	try {
+		Ort::SessionOptions sessionOption;
+		if (useCUDA) {
+			enableCUDA(sessionOption);
+		}
 
-    session_ = new Ort::Session(env, modelName, sessionOption);
-  } catch (const Ort::Exception &exception) {
-    std::cerr << exception.what() << std::endl;
-  }
+		session_ = new Ort::Session(env, modelName, sessionOption);
+	}
+	catch (const Ort::Exception& exception) {
+		std::cerr << exception.what() << std::endl;
+	}
 }
 
-Ort::Session *ofxOrt::getSession() { return session_; }
+Ort::Session* ofxOrt::getSession() { return session_; }
 
-void ofxOrt::forward(const Ort::RunOptions &run_options,
-                     const char *const *input_names,
-                     const Ort::Value *input_values, size_t input_count,
-                     const char *const *output_names, Ort::Value *output_values,
-                     size_t output_count) {
-  session_->Run(Ort::RunOptions{nullptr}, input_names, input_values,
-                input_count, output_names, output_values, output_count);
+void ofxOrt::forward(const Ort::RunOptions& run_options,
+	const char* const* input_names,
+	const Ort::Value* input_values, size_t input_count,
+	const char* const* output_names, Ort::Value* output_values,
+	size_t output_count) {
+	session_->Run(Ort::RunOptions{ nullptr }, input_names, input_values,
+		input_count, output_names, output_values, output_count);
 }
 
-void ofxOrt::enableCUDA(Ort::SessionOptions &options) {
-  OrtCUDAProviderOptions cuda_option = OrtCUDAProviderOptions{0};
-  options.AppendExecutionProvider_CUDA(cuda_option);
+std::vector<Ort::Value> ofxOrt::forward(const Ort::RunOptions& run_options,
+	const char* const* input_names, const Ort::Value* input_values,
+	size_t input_count, const char* const* output_names,
+	size_t output_count) {
+	auto result = session_->Run(Ort::RunOptions{ nullptr }, input_names, input_values,
+		input_count, output_names, output_count);
+	return result;
+}
+
+void ofxOrt::enableCUDA(Ort::SessionOptions& options) {
+	OrtCUDAProviderOptions cuda_option = OrtCUDAProviderOptions();
+	options.AppendExecutionProvider_CUDA(cuda_option);
 }
 
 size_t ofxOrt::getSessionInputCount() const {
--- src/ofxOrt.h
+++ src/ofxOrt.h
@@ -7,8 +7,18 @@
 
 class ofxOrt {
 public:
-  ofxOrt(const ORTCHAR_T *modelName, bool useCUDA);
-  Ort::Session *getSession();
+	ofxOrt(const ORTCHAR_T* modelName, bool useCUDA);
+	Ort::Session* getSession();
+
+	void forward(const Ort::RunOptions& run_options,
+		const char* const* input_names, const Ort::Value* input_values,
+		size_t input_count, const char* const* output_names,
+		Ort::Value* output_values, size_t output_count);
+
+	std::vector<Ort::Value> forward(const Ort::RunOptions& run_options,
+		const char* const* input_names, const Ort::Value* input_values,
+		size_t input_count, const char* const* output_names,
+		size_t output_count);
 
 
 
